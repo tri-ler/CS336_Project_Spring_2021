1f86612a5224c0f4714e54d5b216e272eb3188d8
tri-ler <47867303+tri-ler@users.noreply.github.com>
2021-04-13 11:42:25 -0400

    added bids and items tables

--- createDatabase.sql
+++ createDatabase.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `cs336project` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `cs336project`;
 -- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
 --
 -- Host: localhost    Database: cs336project
@@ -16,6 +14,69 @@ USE `cs336project`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bids`
+--
+
+DROP TABLE IF EXISTS `bids`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bids` (
+  `IDItem` int NOT NULL,
+  `IDBID` int NOT NULL,
+  `bidDate` date DEFAULT NULL,
+  `user` varchar(45) DEFAULT NULL,
+  `isWinning` tinyint DEFAULT NULL,
+  `bidAmount` float DEFAULT NULL,
+  PRIMARY KEY (`IDItem`,`IDBID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bids`
+--
+
+LOCK TABLES `bids` WRITE;
+/*!40000 ALTER TABLE `bids` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bids` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `items` (
+  `IDItem` int NOT NULL,
+  `nameItem` varchar(45) DEFAULT NULL,
+  `userCreated` varchar(45) DEFAULT NULL,
+  `att1Name` varchar(45) DEFAULT NULL,
+  `att1Description` varchar(45) DEFAULT NULL,
+  `att2Name` varchar(45) DEFAULT NULL,
+  `att2Description` varchar(45) DEFAULT NULL,
+  `att3Name` varchar(45) DEFAULT NULL,
+  `att3Description` varchar(45) DEFAULT NULL,
+  `att4Name` varchar(45) DEFAULT NULL,
+  `att4Description` varchar(45) DEFAULT NULL,
+  `highestBidder` varchar(45) DEFAULT NULL,
+  `currentPrice` float DEFAULT NULL,
+  `startDate` date DEFAULT NULL,
+  `endDate` date DEFAULT NULL,
+  PRIMARY KEY (`IDItem`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `representatives`
@@ -61,7 +122,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('','');
+INSERT INTO `users` VALUES ('',''),('user1','user1');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -74,4 +135,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-24  5:19:39
+-- Dump completed on 2021-04-13 11:42:08
