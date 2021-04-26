f722fa453498a7b050de463fd5c7abd8b86b7d82
tri-ler <47867303+tri-ler@users.noreply.github.com>
2021-04-25 20:00:25 -0400

    update 2.0

--- createDatabase.sql
+++ createDatabase.sql
@@ -96,7 +96,7 @@ CREATE TABLE `bids` (
   KEY `username` (`username`),
   KEY `auctionID` (`auctionID`),
   CONSTRAINT `bids_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`),
-  CONSTRAINT `bids_ibfk_2` FOREIGN KEY (`auctionID`) REFERENCES `auction` (`auctionID`) ON DELETE CASCADE)
+  CONSTRAINT `bids_ibfk_2` FOREIGN KEY (`auctionID`) REFERENCES `auction` (`auctionID`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
